0415e64a8e5f6cf1181d187fb30e18736d0bac02
Gentry Demchak <deevolutionism@gmail.com>
2016-08-10 00:44:38 -0400

    styling changes

--- src/index.js
+++ src/index.js
@@ -39,25 +39,28 @@ class Index extends Component {
     if(this.state.projects != null){
       console.log(typeof this.state.projects);
       console.log(this.state.projects.projects.length);
-      // for(var i = 0; i<this.state.projects.length;i++){
-      //   console.log(this.state.projects[i].title);
-      // //   portfolioItems.push(
-      // //     <div className="segment" key={projects.projects[i].title}>
-      // //       <img src={projects.projects[i].image}/>
-      // //       <h2>{projects.projects[i].title}</h2>
-      // //       <h3>{projects.projects[i].type}</h3>
-      // //       <h3>{projects.projects[i].date}</h3>
-      // //       <p>{projects.projects[i].description}</p>
-      // //     </div>
-      // //   );
-      // // }
-      // }
+      for(var i = 0; i<this.state.projects.projects.length;i++){
+        portfolioItems.push(
+          <div className="segment" key={i}>
+            <img className="project_image" src={this.state.projects.projects[i].image}/>
+            <h2>{this.state.projects.projects[i].title}</h2>
+            <h3>{this.state.projects.projects[i].type}</h3>
+            <h3>{this.state.projects.projects[i].date}</h3>
+            <p>{this.state.projects.projects[i].description}</p>
+          </div>
+        );
+      }
     }
 
 
     return (
       <div>
-        <h1>Gentry Demchak</h1>
+        <ul>
+          <li><h1>Gentry Demchak</h1></li>
+          <li><a href="https://github.com/deevolutionism"><img src="github-64.png"/></a></li>
+          <li><a href="https://twitter.com/gdemchak17"><img src="twitter-64.png"/></a></li>
+          <li><a href="https://www.linkedin.com/in/gentry-demchak-843a6a79"><img src="linkedin-64.png"/></a></li>
+        </ul>
         {portfolioItems}
       </div>
     )
